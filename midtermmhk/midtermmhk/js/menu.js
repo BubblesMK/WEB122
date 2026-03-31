8d9ffda0b730ae6448b3295fac2c8f16f679d757
Mandisa Harris <codewithmandisa@gmail.com>
2026-03-31 09:33:54 -0500

    Delete midtermmhk directory

--- midtermmhk/midtermmhk/js/menu.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('toggle-menu'); //get the hamburger menu button element by its ID
-
-button.addEventListener('click', function(event) {
-    event.preventDefault(); 
-
-    const menu = document.getElementById('main-menu');
-    menu.classList.toggle('is-open'); 
-})
